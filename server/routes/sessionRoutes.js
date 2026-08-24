aa869c54ea14d707e1c72cb3ed9988df194274b0
ItsSaahil <sahilpotale2004@gmail.com>
2026-08-24 21:34:17 +0530

    fix student model import case

--- server/routes/sessionRoutes.js
+++ server/routes/sessionRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const Session = require("../models/session");
-const Student = require("../models/students");
+const Student = require("../models/Students");
 
 const router = express.Router();
 
